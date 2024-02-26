aadf7d40c6b84a8875867d712855a9bf6995090e
slyeet03 <145686556+slyeet03@users.noreply.github.com>
2024-02-26 17:04:41 +0530

    Update magnet_link_finder.py

--- scripts/magnet_link_finder.py
+++ scripts/magnet_link_finder.py
@@ -22,20 +22,18 @@ driver = webdriver.Firefox(options=options)
 def search_torrent(query):
 	url = 'https://pirate-proxy.dad/search.php?q='+query+'&all=on&search=Pirate+Search&page=0&orderby='
 	headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64)'}
-
 	try:
 		time.sleep(3)
 		driver.get(url)
 		driver.implicitly_wait(10)
 		html_source = driver.page_source
 		soup = bs(html_source, 'html.parser')
-
+		
 		if(soup.find_all('span', class_="list-item item-name item-title")):
 			results = soup.find_all('span', class_="list-item item-name item-title")
 			size = soup.find_all('span', class_='list-item item-size')
 			magnet_link = search_result(soup, results, size)
 			return magnet_link
-
 		else:	
 			print("No results were returned. Please refine your search.")
 			main.menu()
@@ -48,14 +46,12 @@ def search_result(soup, results, size):
 	links = []
 	i = 1
 	counter = 0
-
 	# showing the results
 	for r,s in zip(results,size):
 		if counter < 20:
 			print(i,re.sub(r'[\W_]+', ' ', r.text[:49]),s.text.replace("B","B, seedrs-"))
 			i += 1
 			counter += 1
-
 	# getting the links for the results
 	for link in soup.find_all('a'):
 		b = link.get('href')
@@ -63,10 +59,9 @@ def search_result(soup, results, size):
 			full_link = "https://pirate-proxy.dad" + b
 			links.append(full_link)
 	
-
 	print("Select a torrent: ")
 	choice = int(input())
-
+	
 	choice_url = links[choice-1]
 	magnet_link = getTorrent(choice_url)
 	return magnet_link
@@ -79,11 +74,10 @@ def getTorrent(choice_url):
 	driver.implicitly_wait(10)
 	html_source = driver.page_source
 	soup = bs(html_source, 'html.parser')
-	
 	desired_label = soup.find('label', id = 'd')
+	
 	if desired_label:
 		magnet=desired_label.find_all('a')
-
 		# finding the magnet link in the webpage
 		for link in magnet:
 			b=link.get('href')
@@ -91,7 +85,6 @@ def getTorrent(choice_url):
 				magnet_link=b
 				return magnet_link
 				driver.quit()
-
 	else:
 		print("No magnet link found")
 		driver.quit()
